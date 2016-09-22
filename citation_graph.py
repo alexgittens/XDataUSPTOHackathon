f665305cff059efe697dd2eb40f0a3caf8c79d36
Keith Levin <keith@Steinhaus.local>
2016-09-22 13:23:50 -0400

    Added code so that we do  on nodes that cite the same work.

--- citation_graph.py
+++ citation_graph.py
@@ -27,7 +27,7 @@ class citation_graph(object):
         """
 
         # Read the data frame in.
-        print 'Unpickling...'
+        print 'Unpickling...',
         try:
             df = pd.read_pickle( picklefile )
         except TypeError:
@@ -40,14 +40,19 @@ class citation_graph(object):
 
         # Build the inverse lookup tables for row indices in the dataframe
         # based on patent and application document ids
+        print 'Building lookup tables...',
         self.app2row = self.build_LUT( df, "patentApplicationDocNumber")
         self.grant2row = self.build_LUT( df, "publishedPatentDocNumber")
+        # Maps a docnumber to a set of rows that cite that docnumber.
+        self.citation_lookup = dict()
+        print 'done.'
         
         # Go thru the rows of the data frame.
         self.neighbor_lookup = dict()
         self.ipcrtype = dict()
         self.app_numbers = dict()
         self.grant_numbers = dict()
+        print 'Processing nodes',
         for rownum in range(nrows):
             if not self.is_vertex(rownum):
                 self.add_vertex( rownum )
@@ -62,6 +67,9 @@ class citation_graph(object):
             self.ipcrtype[rownum] = thisrow['ipcrType']
             self.app_numbers[rownum] = thisrow['patentApplicationDocNumber']
             self.grant_numbers[rownum] = thisrow['publishedPatentDocNumber']
+            if rownum % 1000 == 0:
+                print '.',
+        print 'done.'
 
     def process_citations(self, rownum, citelist):
         """
@@ -69,12 +77,20 @@ class citation_graph(object):
         given list of citations.
         """
         for cite in citelist:
+            if cite not in self.citation_lookup.keys():
+                self.citation_lookup[cite] = set()
+            # Create edges to any other docs that this cites.
             if cite in self.app2row:
                 tgtid = self.app2row[cite]
                 self.add_edge( rownum, tgtid )
             elif cite in self.grant2row:
                 tgtid = self.grant2row[cite]
                 self.add_edge( rownum, tgtid )
+            # Form edges with any other nodes that cite the same citation.
+            for v in self.citation_lookup[cite]:
+                self.add_edge( rownum, v )
+            # And want to map cite back to this row, since this row cites it. 
+            self.citation_lookup[cite].add(rownum)
 
     def is_vertex( self, vx ):
         """
@@ -104,9 +120,10 @@ class citation_graph(object):
             self.add_vertex(vxbb)
 
         # Now actually add the edge.
-        self.neighbor_lookup[vxaa].add( vxbb )
-        self.neighbor_lookup[vxbb].add( vxaa )
-        self.edge_count = self.edge_count + 1
+        if vxbb not in self.get_neighbors(vxaa):
+            self.neighbor_lookup[vxaa].add( vxbb )
+            self.neighbor_lookup[vxbb].add( vxaa )
+            self.edge_count = self.edge_count + 1
 
     def build_LUT( self, df, attr_name ):
         """
